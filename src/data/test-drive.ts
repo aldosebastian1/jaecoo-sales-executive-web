616ad940fd4e9b509ab996a5e73c86766fdfc115
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-07-28 15:03:27 +0700

    Menambahkan model dan data test drive untuk Jaecoo J7, Jaecoo J7 PHEV, dan Jaecoo J8

---
+++ src/data/test-drive.ts
@@ -0,0 +1,10 @@
+export interface TestDriveModel {
+  id: string;
+  name: string;
+}
+
+export const testDriveModels: TestDriveModel[] = [
+  { id: 'jaecoo_j7', name: 'Jaecoo J7' },
+  { id: 'jaecoo_j7_phev', name: 'Jaecoo J7 PHEV' },
+  { id: 'jaecoo_j8', name: 'Jaecoo J8' }
+];
