71bb39583449a48af21728c1c3599ec9e5bd00dd
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-08-01 21:36:41 +0700

    Update button hover color in error component for improved UI feedback

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -46,7 +46,7 @@ export default function Error({
           <div className="flex flex-col sm:flex-row gap-4 w-full sm:w-auto justify-center items-center mt-4">
             <button 
               onClick={() => reset()}
-              className="font-geist bg-primary text-white px-7 py-3 text-[16px] font-semibold tracking-[0.02em] rounded-full w-full sm:w-auto text-center hover:bg-[#003399] transition-all duration-300"
+              className="font-geist bg-primary text-white px-7 py-3 text-[16px] font-semibold tracking-[0.02em] rounded-full w-full sm:w-auto text-center hover:bg-[#155464] transition-all duration-300"
             >
               MUAT ULANG
             </button>
