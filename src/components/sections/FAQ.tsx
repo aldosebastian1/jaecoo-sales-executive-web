4543d188e157ffd0708698c2bb0db60860790092
Aldo Sebastian <emailaldosebastian@gmail.com>
2026-08-02 23:03:33 +0700

    Update FAQ component image source to helpdesk

--- src/components/sections/FAQ.tsx
+++ src/components/sections/FAQ.tsx
@@ -209,7 +209,7 @@ export default function FAQ() {
             {/* Right Side: Image */}
             <div className="relative z-10 w-full xl:w-[22%] shrink-0 h-[100px] rounded-xl overflow-hidden block">
               <Image 
-                src="/images/card.jpg"
+                src="/images/helpdesk.jpg"
                 alt="Jaecoo Helpdesk"
                 fill
                 className="object-cover object-center"
